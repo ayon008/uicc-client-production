764a7bc5c86b439ae5e1495cf24615a5f6662a88
ayon008 <shariar.ayon128@gmail.com>
2024-12-23 17:34:26 +0600

    carrer

--- src/app/career-counseling/page.js
+++ src/app/career-counseling/page.js
@@ -11,7 +11,7 @@ import ServiceBanner from '@/shared/ServiceBanner';
 const Page = () => {
     return (
         <div>
-            <ServiceBanner text1={'Career Counseling'} img={img1} />
+            <ServiceBanner text1={'Career'} text2={'Counseling'} img={img1} />
             <div className='2xl:p-20 xl:p-16 p-12'>
                 <h1 className='2xl:text-5xl xl:text-4xl font-semibold'>Career Counseling at UICC</h1>
                 <p className='2xl:text-xl xl:text-lg font-normal mt-6'>
@@ -80,9 +80,7 @@ const Page = () => {
                         </span>
                     </li>
                 </ul>
-                <h2 className='2xl:text-5xl xl:text-4xl font-semibold my-10'>
-                    Job Market Preparation
-                </h2>
+
                 <div className='flex 2xl:flex-row xl:flex-row flex-col items-center mt-10 2xl:gap-6 xl:gap-6 gap-3 w-full'>
                     <div className='2xl:w-1/2 xl:w-1/2 w-full'>
                         <Image placeholder='blur' className='2xl:flex-1 xl:flex-1 w-full 2xl:h-[450px] xl:h-[350px] 350px object-cover' src={img1} alt='' />
@@ -91,6 +89,11 @@ const Page = () => {
                         <Image placeholder='blur' className='2xl:flex-1 xl:flex-1 w-full 2xl:h-[450px] xl:h-[350px] 350px object-cover' src={img2} alt='' />
                     </div>
                 </div>
+
+                <h2 className='2xl:text-5xl xl:text-4xl font-semibold my-10'>
+                    Job Market Preparation
+                </h2>
+
                 <ul className='space-y-10'>
                     <li className='flex items-center'>
                         <TickIcon />
